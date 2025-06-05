7f95e04397573a63866076bdb522663aad0258cc
Kazys <kazys2006rtt@gmail.com>
2025-06-05 13:25:40 -0300

    Mostrar o Slide

--- SlideShow/SlideShow.js
+++ SlideShow/SlideShow.js
@@ -5,3 +5,11 @@ const dotsContainer = document.getElementById('dots');
 
 let currentSlide = 0;
 let slideInterval;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    dots.forEach(dot => dot.classList.remove('active'));
+
+    slides[index].classList.add('active');
+    dots[index].classList.add('active');
+}
