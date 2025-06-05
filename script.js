3fd7b7ab20a708792a2f30dc70c50ee685ace4da
Kazys <kazys2006rtt@gmail.com>
2025-06-04 22:48:05 -0300

    EventListener CLICK para o Hamburguer MENU

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const hamburger = document.getElementById('hamburger');
 const navMobile = document.getElementById('nav-mobile');
 const closeBtn = document.getElementById('close-btn');
+
+hamburger.addEventListener('click', () => {
+    navMobile.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    navMobile.classList.remove('active');
+});
